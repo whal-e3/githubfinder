b0d853d65c66eaa6047a3df92b2ff3eadade1ea5
eric8979 <buckelwal8979@khu.ac.kr>
2021-02-02 18:07:23 +0900

    upload file

---
+++ github.js
@@ -0,0 +1,26 @@
+class Github {
+	constructor() {
+		this.client_id = 'bae776441abaec89bf3d';
+		this.client_secret = '5e43b225466615e433343eb1163525803c1e9e50';
+		this.repos_count = 5;
+		this.repos_sort = 'created: asc';
+	}
+
+	async getUser(user) {
+		const profileResponse = await fetch(
+			`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`
+		);
+
+		const repoResponse = await fetch(
+			`https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`
+		);
+
+		const profile = await profileResponse.json();
+		const repos = await repoResponse.json();
+
+		return {
+			profile,
+			repos
+		};
+	}
+}
